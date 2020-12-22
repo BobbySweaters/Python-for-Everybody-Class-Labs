ae1c17d167ac68d4e2133b41ade49239ea46e23a
BobbySweaters <rcharlesholland@outlook.com>
2020-12-22 16:48:54 -0600

    This is the initial commit

---
+++ Introduction Screenshot.py
@@ -0,0 +1,6 @@
+
+#This is a test file for "Programming for Everybody; Week 2"
+
+print("Don't you love my text editor?")
+
+print("I'm excited to take this class")
---
+++ readme.md
@@ -0,0 +1,1 @@
+This is the readme file for "Programming for Everybody". This respository is just for assignment modules.
